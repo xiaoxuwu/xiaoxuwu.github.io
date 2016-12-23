7ce640c2aee5d9f156e172e295b3da36699cb3e2
Carter Wu <carter.wu@ucla.edu>
2016-12-23 11:09:11 -0800

    finish putting menu into js file

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,23 @@
+/* code for determining active */
+var isBioSelected = document.URL.search("bio") != -1 ? 'class = selected' : '';
+var isPortSelected = document.URL.search("portfolio") != -1 ? 'class = selected' : '';
+var isContactSelected = document.URL.search("contact") != -1 ? 'class = selected' : '';
+var isHomeSelected = (isBioSelected + isPortSelected + isContactSelected).length == 0 ? 'class = selected' : '';
+
+/* main nav */
+document.getElementById("NavMenu").innerHTML =
+'<button class="cn-button" id="cn-button">Menu</button>' +
+'<div class="cn-wrapper" id="cn-wrapper">' +
+	'<ul>' +
+		'<li><a ' + isHomeSelected + ' href="/"><span>Home</span></a></li>' +
+		'<li><a ' + isBioSelected +' href="/bio"><span>Bio</span></a></li>' +
+		'<li><a ' + isPortSelected + ' href="/portfolio"><span>Portfolio</span></a></li>' +
+		'<li><a ' + isContactSelected + ' href="/contact"><span>Contact</span></a></li>' +
+	'</ul>' +
+'</div>' +
+'<div id="cn-overlay" class="cn-overlay"></div>';
+
+/* Oher Effects Below */
 (function(){
 
 	var button = document.getElementById('cn-button'),
@@ -26,17 +46,16 @@
 	}
 	function openNav(){
 		open = true;
-	    button.innerHTML = "-";
+	    button.innerHTML = "Close";
 	    classie.add(overlay, 'on-overlay');
 	    classie.add(wrapper, 'opened-nav');
 	}
 	function closeNav(){
 		open = false;
-		button.innerHTML = "+";
+		button.innerHTML = "Menu";
 		classie.remove(overlay, 'on-overlay');
 		classie.remove(wrapper, 'opened-nav');
 	}
 	document.addEventListener('click', closeNav);
 
 })();
-
